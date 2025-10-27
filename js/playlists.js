068a648fd09b165f78630c6bf298014520b108c8
victorozzyy <victorozzyy@gmail.com>
2025-10-27 15:00:36 -0300

    Update playlists.js

--- js/playlists.js
+++ js/playlists.js
@@ -28,6 +28,7 @@ const PlaylistModule = {
     
     // Playlists locais
     availablePlaylists: [
+        { name: "Lista01", filename: "lsta01.m3u8" },
         { name: "24 Hs", filename: "playlist_24h.m3u" }
     ],
     
@@ -633,6 +634,7 @@ if (typeof module !== 'undefined' && module.exports) {
     module.exports = PlaylistModule;
 
 }
+
 
 
 
